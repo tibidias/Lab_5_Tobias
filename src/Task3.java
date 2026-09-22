a1b22de0202b4225ba74e91cd80604d086f09a9a
tibidias <baker.616696@swl.k12.oh.us>
2026-09-22 11:43:50 -0400

    Task3.java

--- src/Task3.java
+++ src/Task3.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class Task3 {
+class Task3{
+    static void main() {
+        Scanner scan = new Scanner(System.in);
+        String userInput;
+
+        System.out.println("What political party are you affiliated with (D, R, I).");
+
+        if (scan.hasNextLine()) {
+            userInput = scan.nextLine();
+            if (userInput.equalsIgnoreCase("D")) {
+                System.out.println("You get a Democratic Donkey.");
+            } else if (userInput.equalsIgnoreCase("R")) {
+                System.out.println("You get a Republican Elephant.");
+            } else if (userInput.equalsIgnoreCase("I")) {
+                System.out.println("You get an independant person.");
+            } else {
+                System.out.println("Please input D, R, or I.");
+            }
+        } else {
+            System.out.println("Please input D, R, or I.");
+        }
+    }
 }
