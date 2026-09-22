1fad2486892e5a5d3df31c60768feda6bb0589ce
tibidias <baker.616696@swl.k12.oh.us>
2026-09-22 11:44:06 -0400

    Task2.java

--- src/Task2.java
+++ src/Task2.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class Task2 {
+class Task2{
+    static void main() {
+        Scanner scan = new Scanner(System.in);
+        int userMonth;
+
+        System.out.println("What month is your birthday(1-12).");
+
+        if (scan.hasNextInt()){
+            userMonth = scan.nextInt();
+            if(userMonth >=1 && userMonth <= 12) {
+                System.out.println("Your birth month is :" + userMonth + ".");
+            }else {
+                System.out.println("Enter a whole number between 1 and 12.");
+            }
+        }else {
+            System.out.println("Enter a whole number between 1 and 12.");
+        }
+    }
 }
